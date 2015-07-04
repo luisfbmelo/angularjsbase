2ccc49fa338b6b596c68b4fb4c9ae51eb088f7e3
Luis Melo <geral@luisfbmelo.com>
2015-07-04 18:20:41 +0000

    Save all uncommited

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,22 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		sass: {
+		compass: {                  // Task
+		    dist: {                   // Target
+		      options: {              // Target options
+		        sassDir: 'assets/styles/sass',
+		        cssDir: 'assets/styles/stylesheets',
+		        environment: 'production'
+		      }
+		    },
+		    dev: {                    // Another target
+		      options: {
+		        sassDir: 'assets/styles/sass',
+		        cssDir: 'assets/styles/stylesheets/dev'
+		      }
+		    }
+	  	},	
+		/*sass: {
 			dist: {
 				options: {
 			      style: 'compressed'
@@ -10,7 +25,7 @@ module.exports = function(grunt) {
 					'assets/styles/screen.css' : 'assets/styles/sass/screen.scss'
 				}
 			}
-		},
+		},*/
 		concat: {
 			js: {
                 src: [ 
@@ -33,31 +48,37 @@ module.exports = function(grunt) {
 			    }
 		    }
 		},
-		cssmin: {
+		/*cssmin: {
 		  target: {
 		    files: [{
 		      expand: true,
-		      cwd: 'assets/styles',
+		      cwd: 'assets/styles/stylesheets',
 		      //src: ['*.css', '!*.min.css'],
 		      src: ['screen.css'],
-		      dest: 'assets/styles',
+		      dest: 'assets/styles/stylesheets',
 		      ext: '.min.css'
 		    }]
 		  }
-		},
+		},*/
 		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass','cssmin']
+			compass: {
+				files: ['**/*.{scss,sass}'],
+				tasks: ['compass']
 			},
+			//css: {
+				//files: '**/*.css',
+				//tasks: ['sass','cssmin']
+				//tasks: ['cssmin']
+			//},
 			concat: {
 			    files: '**/*.js',
 			    tasks: ['concat', 'uglify']
 			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-sass');	
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	//grunt.loadNpmTasks('grunt-contrib-sass');	
+	//grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
